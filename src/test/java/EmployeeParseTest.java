35bcc57a9d9dd9e48df94df83e7b9ceac0d7b366
Vsevolod21 <kononenko.vsevolod@gmail.com>
2022-04-08 11:48:08 +0300

    Added parameterizedTest commit

--- src/test/java/EmployeeParseTest.java
+++ src/test/java/EmployeeParseTest.java
@@ -3,31 +3,36 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class EmployeeParseTest extends TestCase {
     EmployeeParse employeeParse;
-    List<Employee> expectedList;
-    List<Employee> listJson;
+    List<Employee> expectedListCSVAndXML;
+    List<Employee> listToJsonAndJsonToList;
 
     @BeforeEach
     public void setUp() {
         employeeParse = new EmployeeParse();
-        expectedList = new ArrayList<>();
-        expectedList.add(new Employee(1, "John", "Smith", "USA", 25));
-        expectedList.add(new Employee(2, "Inav", "Petrov", "RU", 23));
-        listJson = new ArrayList<>();
-        listJson.add(new Employee(1, "John", "Smith", "USA", 25));
+
+        expectedListCSVAndXML = new ArrayList<>();
+        expectedListCSVAndXML.add(new Employee(1, "John", "Smith", "USA", 25));
+        expectedListCSVAndXML.add(new Employee(2, "Inav", "Petrov", "RU", 23));
+
+        listToJsonAndJsonToList = new ArrayList<>();
+        listToJsonAndJsonToList.add(new Employee(1, "John", "Smith", "USA", 25));
     }
     @AfterEach
     public void tearDown() {
         employeeParse = null;
-        expectedList = null;
-        listJson = null;
+        expectedListCSVAndXML = null;
+        listToJsonAndJsonToList = null;
     }
 
     @Test
@@ -36,14 +41,26 @@ public class EmployeeParseTest extends TestCase {
         String fileNameCSV = "data.csv";
         final List<Employee> resultList = employeeParse.parseCSV(columnMapping, fileNameCSV);
         Assertions.assertNotNull(resultList);
-        Assertions.assertEquals(expectedList,resultList);
+        Assertions.assertEquals(expectedListCSVAndXML,resultList);
+    }
+
+    @ParameterizedTest
+    @MethodSource("expectedFactory")
+    public void testListToJsonParams(String expectedStringJson) {
+        final String resultStringJson = employeeParse.listToJson(listToJsonAndJsonToList);
+        Assertions.assertNotNull(resultStringJson);
+        Assertions.assertEquals(expectedStringJson, resultStringJson);
+    }
+    private static Stream<String> expectedFactory() {
+        return Stream.of("[{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}]",
+                "[{\"id\":2,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}]");
     }
 
     @Test
     public void testListToJson() {
         final String expectedStringJson =
                 "[{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}]";
-        final String resultStringJson = employeeParse.listToJson(listJson);
+        final String resultStringJson = employeeParse.listToJson(listToJsonAndJsonToList);
         Assertions.assertNotNull(resultStringJson);
         Assertions.assertEquals(expectedStringJson, resultStringJson);
     }
@@ -53,7 +70,7 @@ public class EmployeeParseTest extends TestCase {
         final String fileNameXML = "data.xml";
         final List<Employee> resultList = employeeParse.parseXML(fileNameXML);
         Assertions.assertNotNull(resultList);
-        Assertions.assertEquals(expectedList, resultList);
+        Assertions.assertEquals(expectedListCSVAndXML, resultList);
     }
 
     @Test
@@ -65,12 +82,12 @@ public class EmployeeParseTest extends TestCase {
         final String resultJsonString = employeeParse.readString(fileName);
         Assertions.assertEquals(expectedJsonString, resultJsonString);
     }
-    
+
     @Test
     public void testJsonToList() {
         final String jsonString =
                 "[{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}]";
         final List<Employee> resultJsonList = employeeParse.jsonToList(jsonString);
-        Assertions.assertEquals(listJson, resultJsonList);
+        Assertions.assertEquals(listToJsonAndJsonToList, resultJsonList);
     }
 }
